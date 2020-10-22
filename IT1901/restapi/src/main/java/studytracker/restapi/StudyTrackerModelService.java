3d79aa4bd65bc457d1dcc139c451a8a4c1068b95
Rasmus Longva Haugland <rasmusha@stud.ntnu.no>
2020-10-22 09:02:25 +0000

    #24 - fixed pom in parent and restapi

--- IT1901/restapi/src/main/java/studytracker/restapi/StudyTrackerModelService.java
+++ IT1901/restapi/src/main/java/studytracker/restapi/StudyTrackerModelService.java
@@ -1,23 +1,34 @@
 package studytracker.restapi;
-@Path(StudyTrackerModelService.STUDYTRACKER_MODEL_SERVICE_PATH)
-public class StudyTrackerModelService {
-  public static final STUDYTRACKER_MODEL_SERVICE_PATH = 'studyTracker';
 
-  @inject
-  private StudyTrackerModel studyTrackerModel;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+//@Path(StudyTrackerModelService.STUDYTRACKER_MODEL_SERVICE_PATH)
+public class StudyTrackerModelService {
+  
+  // private static final Logger LOG = LoggerFactory.getLogger(StudyTrackerModelService.class);
+  // public static final String STUDYTRACKER_MODEL_SERVICE_PATH = "studyTracker";
+
+  // @inject
+  // private StudyTrackerModel studyTrackerModel;
   
 
-  @GET
-  @Produces(MediaType.APPLICTION_JSON)
-  public StudyTrackerModel getStudyTrackerModel() {
-      return this.studyTrackerModel;
-  }
-  @Path('/{name}')
-  public StudyTrackerResource getStudyTrackerList(@PathParam('name') String name) {
-    Semester semester = getStudyTrackerModel
-  }
-
-}
-
-
+  // @GET
+  // @Produces(MediaType.APPLICTION_JSON)
+  // public StudyTrackerModel getStudyTrackerModel() {
+  //     return this.studyTrackerModel;
+  // }
+  // @Path("/{name}")
+  // public StudyTrackerResource getStudyTrackerList(@PathParam("name") String name) {
+  //   Semester semester = getStudyTrackerModel().getStudyTrackerList();
+  //   LOG.debug("Sub-resource for TodoList " + name + ": " + semester);
+  //   return new TodoListResource(studyTrackerModel, name, semester);
+  // }
 }
