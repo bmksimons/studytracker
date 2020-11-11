86ddaa0f2b2142677df0fd5c2158e2c5e39d67d6
bmsimons <bmsimons@stud.ntnu.no>
2020-11-11 00:22:07 +0000

    #34 - Started on the tests for the restapi and restserver

---
+++ IT1901/integrationtests/src/test/java/studytracker/ui/StudyTrackerAppIT.java
@@ -0,0 +1,7 @@
+
+
+public class StudyTrackerAppIT {
+
+  //se på trætteberg og lag trster for metoder i remotesemesteeraccess?? tror jeg
+  
+}
---
+++ IT1901/restserver/src/test/java/restserver/StudyTrackerServiceTest.java
@@ -0,0 +1,102 @@
+package restserver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import studytracker.core.Course;
+import studytracker.core.Semester;
+import studytracker.restapi.SemesterService;
+import studytracker.restserver.StudyTrackerConfig;
+import studytracker.restserver.StudyTrackerModuleObjectMapperProvider;
+
+public class StudyTrackerServiceTest extends JerseyTest {
+
+  protected boolean shouldLog() {
+    return false;
+  }
+
+  @Override
+  protected ResourceConfig configure() {
+    final StudyTrackerConfig config = new StudyTrackerConfig();
+    if (shouldLog()) {
+      enable(TestProperties.LOG_TRAFFIC);
+      enable(TestProperties.DUMP_ENTITY);
+      config.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, "WARNING");
+    }
+    return config;
+  }
+
+  @Override
+  protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+    return new GrizzlyTestContainerFactory();
+  }
+
+  private ObjectMapper objectMapper;
+
+  @Override
+  @BeforeEach
+  public void setUp() throws Exception {
+    super.setUp();
+    objectMapper = new StudyTrackerModuleObjectMapperProvider().getContext(getClass());
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  @Test
+  public void testGet_todo() {
+    Response getResponse = target(SemesterService.STUDYTRACKER_MODEL_SERVICE_PATH)
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+        .get();
+    assertEquals(200, getResponse.getStatus());
+    try {
+      Semester semester = objectMapper.readValue(getResponse.readEntity(String.class), Semester.class);
+      // Iterator<Course> it = todoModel.iterator();
+      // assertTrue(it.hasNext());
+      // AbstractTodoList todoList1 = it.next();
+      // assertTrue(it.hasNext());
+      // AbstractTodoList todoList2 = it.next();
+      // assertFalse(it.hasNext());
+      // assertEquals("todo1", todoList1.getName());
+      // assertEquals("todo2", todoList2.getName());
+    } catch (JsonProcessingException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testGetCourse() {
+    Response getResponse = target(SemesterService.STUDYTRACKER_MODEL_SERVICE_PATH)
+        .path("todo1")
+        .request(MediaType.APPLICATION_JSON + ";" + MediaType.CHARSET_PARAMETER + "=UTF-8")
+        .get();
+    assertEquals(200, getResponse.getStatus());
+    try {
+      Course course = objectMapper.readValue(getResponse.readEntity(String.class), Course.class);
+      assertEquals("todo1", course.getCourseName());
+    } catch (JsonProcessingException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  //lage tester for delete og put og post
+}
