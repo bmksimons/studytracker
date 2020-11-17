f9638fdc48e091bd859e314d870ba2069e289352
bmsimons <bmsimons@stud.ntnu.no>
2020-11-17 09:24:37 +0000

    #34 - finished making the integrationtests

--- IT1901/integrationtests/src/test/java/studytracker/ui/AppIT.java
+++ IT1901/integrationtests/src/test/java/studytracker/ui/AppIT.java
@@ -3,17 +3,11 @@ package studytracker.ui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.print.DocFlavor.URL;
-
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
@@ -32,17 +26,8 @@ public class AppIT extends ApplicationTest{
   }
 
   @BeforeEach
-  public void setupItems() throws URISyntaxException {
-    try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("it_semester.json"))) {
-      String port = System.getProperty("studytracker.port");
-      assertNotNull(port, "No studytracker.port system property set");
-      URI baseUri = new URI("http://localhost:" + port + "/studytracker/");
-      System.out.println("Base RemoteTodoModelAcces URI: " + baseUri);
-      this.controller.setRemoteSemesterAccess(new RemoteSemesterAccess(baseUri));
+  public void setup(){
       clickOn("#reset");
-    } catch (IOException e) {
-      fail(e.getMessage());
-    }
   }
 
   @Test
@@ -50,60 +35,33 @@ public class AppIT extends ApplicationTest{
     assertNotNull(this.controller);
   }
 
-  // @Test
-  // public void testAddNewCourse() {
-  //   clickOn("#newCourse").write("IT1901");
-  //   clickOn("#addCourse");
-  //   assertEquals(controller.getCourseNames().stream().map(x -> x.getText()).anyMatch(a -> a.equals("IT1901")), true);
-  // }
+  @Test
+  public void testAddNewCourse() {
+    clickOn("#newCourse").write("IT1901");
+    clickOn("#addCourse");
+    assertEquals(controller.getCourseNames().stream().map(x -> x.getText()).anyMatch(a -> a.equals("IT1901")), true);
+  }
 
-  // @Test
-  // public void testAddEqualCourse() {
-  //   clickOn("#newCourse").write("Matte");
-  //   clickOn("#addCourse");
-  //   clickOn("#newCourse").write("Matte");
-  //   assertEquals(controller.getRemoteSemesterAccess().getSemester().getCourses().size(), 1);
-  // }
+  @Test
+  public void testAddEqualCourse() {
+    clickOn("#newCourse").write("Matte");
+    clickOn("#addCourse");
+    clickOn("#newCourse").write("Matte");
+    assertEquals(controller.getRemoteSemesterAccess().getSemester().getCourses().size(), 1);
+  }
 
-  // @Test
-  // public void testAddTimePlusButton() {
-  //   clickOn("#newCourse").write("matte");
-  //   clickOn("#addCourse");
-  //   clickOn("#pickCourse");
-  //   type(KeyCode.DOWN);
-  //   type(KeyCode.ENTER);
-  //   clickOn("#plusTime");
-  //   clickOn("#addTime");
-  //   assertEquals(controller.gettimeSpentOnCoursesList().stream().anyMatch(a -> a.equals("0.25 t")), true);
-
-  // }
-
-  // @Test
-  // public void testAddTimeMinusButton() {
-  //   clickOn("#newCourse").write("matte");
-  //   clickOn("#addCourse");
-  //   clickOn("#pickCourse");
-  //   type(KeyCode.DOWN);
-  //   type(KeyCode.ENTER);
-  //   for (int i = 0; i < 10; i++) {
-  //     clickOn("#plusTime");
-  //   }
-  //   clickOn("#minusTime");
-  //   clickOn("#addTime");
-  //   assertEquals(controller.gettimeSpentOnCoursesList().stream().anyMatch(a -> a.equals("2.25 t")), true);
-  // }
-
-  // @Test
-  // public void testDeleteCourseSimple() {
-  //   clickOn("#newCourse").write("matte1");
-  //   clickOn("#addCourse");
-  //   clickOn("#pickCourseDelete");
-  //   type(KeyCode.DOWN);
-  //   type(KeyCode.ENTER);
-  //   clickOn("#delete");
-  //   try {
-  //     assertEquals(controller.getCourseNames().stream().map(x -> x.getText()).allMatch(a -> a.equals("")), true);
-  //   } catch (NoSuchElementException e) {
-  //   }
-  // }
+  @Test
+  public void testDeleteCourseSimple() {
+    clickOn("#newCourse").write("matte1");
+    clickOn("#addCourse");
+    clickOn("#pickCourseDelete");
+    type(KeyCode.DOWN);
+    type(KeyCode.ENTER);
+    clickOn("#delete");
+    try {
+      assertEquals(controller.getCourseNames().stream().map(x -> x.getText()).allMatch(a -> a.equals("")), true);
+    } catch (NoSuchElementException e) {
+    }
+    clickOn("#reset");
+  } 
 }
