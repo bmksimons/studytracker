92fb59f93389d93626928c86c43b7edf4aa2af07
Rasmus Longva Haugland <rasmusha@stud.ntnu.no>
2020-11-18 10:12:44 +0000

    #49 fixed several checkstyle errors

--- IT1901/fxui/src/main/java/studytracker/ui/ModifyTime.java
+++ IT1901/fxui/src/main/java/studytracker/ui/ModifyTime.java
@@ -12,19 +12,21 @@ public class ModifyTime {
    * Methode for adding time to a course.
    * 
    * @param time take in a string and transform it into a double.
+   * 
    * @return a string with new time +"t" for a given course.
    */
   public String addTime(String time) {
     String[] partition = time.split(Pattern.quote(" "));
     Double currentTime = Double.parseDouble(partition[0]);
     currentTime = currentTime + 0.25;
-    return (currentTime +" h");
+    return (currentTime + " h");
   }
 
   /**
    * Methode for reducing the time given as input.
    * 
-   * @param time , string with current timie.
+   * @param time , string with current time
+   * 
    * @return string with new time.
    */
   public String removeTime(String time) {
@@ -42,16 +44,17 @@ public class ModifyTime {
   /**
    * Method for converting a string into double and calculate studied time.
    * 
-   * @param timeToAdd,currentTime
+   * @param timeToAdd , a string that contains the time you want to add to a course 
+   * @param currentTime a string with the current time spent on a course.
    * @return a list of doubles with three elements; hoursToAdd,
    *         currentTimeStuided, and newTimeStudied which is the sum of the two
    *         other.
    */
   public List<Double> makeStudyHours(String timeToAdd, String currentTime) {
     Double HoursToAdd = Double.parseDouble(timeToAdd.split(Pattern.quote(" "))[0]);
-    Double currentTImeStudied = Double.parseDouble(currentTime.split(Pattern.quote(" "))[0]);
-    Double newTimeStudied = HoursToAdd + currentTImeStudied;
-    List<Double> list = Arrays.asList(HoursToAdd, currentTImeStudied, newTimeStudied);
+    Double currentTimeStudied = Double.parseDouble(currentTime.split(Pattern.quote(" "))[0]);
+    Double newTimeStudied = HoursToAdd + currentTimeStudied;
+    List<Double> list = Arrays.asList(HoursToAdd, currentTimeStudied, newTimeStudied);
     return list;
   }
 }
